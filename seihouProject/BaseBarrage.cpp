2f3a273991500eda3cb2859624bc3b438d928121
itasi29 <135081567+itasi29@users.noreply.github.com>
2023-09-12 12:35:50 +0900

    描画、ハンドル、ポジションのメンバ関数

--- seihouProject/BaseBarrage.cpp
+++ seihouProject/BaseBarrage.cpp
@@ -3,12 +3,20 @@
 
 #include "BaseBarrage.h"
 
+namespace
+{
+	constexpr int kBaseSpeedX = 4;
+	constexpr int kBaseSpeedY = 4;
+}
+
 BaseBarrage::BaseBarrage():
 	m_handle(-1),
+	m_graphSizeX(0),
+	m_graphSizeY(0),
 	m_x(0),
 	m_y(0),
-	m_moveX(0),
-	m_moveY(0)
+	m_speedX(kBaseSpeedX),
+	m_speedY(kBaseSpeedY)
 {
 }
 
@@ -23,9 +31,26 @@ void BaseBarrage::Init()
 void BaseBarrage::Update()
 {
 	// à íuÇÃçXêV
+	m_x += m_speedX;
+	m_y += m_speedY;
 }
 
 void BaseBarrage::Draw() const
 {
-	DrawGraph(m_x, m_y, m_handle, true);
+	DrawGraph(m_x - m_graphSizeX / 2, m_y - m_graphSizeY / 2
+			, m_handle, true);
+}
+
+void BaseBarrage::SetHandle(int handle)
+{
+	m_handle = handle;
+	assert(m_handle != -1);
+
+	GetGraphSize(m_handle, &m_graphSizeX, &m_graphSizeY);
+}
+
+void BaseBarrage::SetPos(int x, int y)
+{
+	m_x = x;
+	m_y = y;
 }
