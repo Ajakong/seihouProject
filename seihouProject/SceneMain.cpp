8f2abfc86772158078fee3eb05d4d96090bbba53
itasi29 <135081567+itasi29@users.noreply.github.com>
2023-09-26 11:06:19 +0900

    コミット

--- seihouProject/SceneMain.cpp
+++ seihouProject/SceneMain.cpp
@@ -8,6 +8,8 @@
 #define _TASHIRO
 #define _YOSIYAMA
 
+#include <DxLib.h>
+
 #include "SceneMain.h"
 #include "Player.h"
 #include "BaseBarrage.h"
@@ -22,46 +24,42 @@ SceneMain::SceneMain():
 	m_bgHandle(-1),
 	m_barrageHandle(-1)
 {
-#ifdef TAKASAKI
+	m_playerHandle = LoadGraph("data/image/player.png");
+	m_enemyHnadle = LoadGraph("data/image/enemy.png");
+	m_enemyItemHandle = LoadGraph("data/image/enemyItem.png");
+	m_bgHandle = LoadGraph("data/image/bg.png");
+	m_barrageHandle = LoadGraph("data/image/barrage.png");
+
 	m_pPlayer = new Player();
 	m_pPlayer->SetHandle(m_playerHandle);
-#endif
-
-#ifdef TASHIRO
 	m_pBaseBarrage = new BaseBarrage();
 	m_pBaseBarrage->SetHandle(m_barrageHandle);
-#endif
-
-#ifdef YOSIYAMA
 	m_pEnemyMove = new EnemyMove();
 	m_pEnemyMove->SetHandle(m_enemyHnadle);
 	m_pEnemyItem = new EnemyItem();
 	m_pEnemyItem->SetHandle(m_enemyItemHandle);
 	m_pBg = new Haikei();
 	m_pBg->SetHandle(m_bgHandle);
-#endif
 }
 
 SceneMain::~SceneMain()
 {
-#ifdef TAKASAKI
+	DeleteGraph(m_playerHandle);
+	DeleteGraph(m_enemyHnadle);
+	DeleteGraph(m_enemyItemHandle);
+	DeleteGraph(m_bgHandle);
+	DeleteGraph(m_barrageHandle);
+
 	delete(m_pPlayer);
 	m_pPlayer = nullptr;
-#endif
-
-#ifdef TASHIRO
 	delete(m_pBaseBarrage);
 	m_pBaseBarrage = nullptr;
-#endif
-
-#ifdef YOSIYAMA
 	delete(m_pEnemyMove);
 	m_pEnemyMove = nullptr;
 	delete(m_pEnemyItem);
 	m_pEnemyItem = nullptr;
 	delete(m_pBg);
 	m_pBg = nullptr;
-#endif
 }
 
 void SceneMain::Init()
