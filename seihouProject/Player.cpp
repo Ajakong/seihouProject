b04e8304a2089b07c1efc68f20cc8ca9459d8626
Ajakong <135081816+Ajakong@users.noreply.github.com>
2023-09-26 10:16:32 +0900

    コミット

--- seihouProject/Player.cpp
+++ seihouProject/Player.cpp
@@ -1,6 +1,14 @@
 #include "Player.h"
 #include"Dxlib.h"
+#include"Pad.h"
 
+Player::Player():
+m_handle(0),
+m_posX(0),
+m_posY(0)
+{
+
+}
 Player::~Player()
 {
 }
@@ -16,9 +24,18 @@ void Player::End()
 
 void Player::Update()
 {
-	
+
+	//padの十字キーを使用してプレイヤーを移動させる
+	int pad = GetJoypadInputState(DX_INPUT_KEY_PAD1);
+
+
+	if (CheckHitKey(KEY_INPUT_UP) == 1) m_posX -= 3;
+	if (CheckHitKey(KEY_INPUT_DOWN) == 1) m_posX += 3;
+	if (CheckHitKey(KEY_INPUT_RIGHT) == 1) m_posY += 3;
+	if (CheckHitKey(KEY_INPUT_LEFT) == 1) m_posY -= 3;
 }
 
 void Player::Draw()
 {
+	DrawCircle(m_posX, m_posY, m_pos + 5, m_posY + 5, GetColor(255, 255, 255);
 }
