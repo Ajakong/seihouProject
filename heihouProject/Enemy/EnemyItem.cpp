72fc67b17c01d9e36cf32e7ed6340b455eb8b2c4
itukikunmeuu <135081677+itukikunmeuu@users.noreply.github.com>
2023-10-16 10:55:29 +0900

    up

---
+++ heihouProject/Enemy/EnemyItem.cpp
@@ -0,0 +1,25 @@
+#include "EnemyItem.h"
+
+EnemyItem::EnemyItem()
+{
+}
+
+EnemyItem::~EnemyItem()
+{
+}
+
+void EnemyItem::Init()
+{
+}
+
+void EnemyItem::End()
+{
+}
+
+void EnemyItem::Update()
+{
+}
+
+void EnemyItem::Draw()
+{
+}
