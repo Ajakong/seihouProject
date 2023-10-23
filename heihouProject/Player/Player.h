dd8c91662f63291d4e7833efc06cb2cd95fd838d
Ajakong <135081816+Ajakong@users.noreply.github.com>
2023-10-23 10:36:31 +0900

    up

--- heihouProject/Player/Player.h
+++ heihouProject/Player/Player.h
@@ -1,5 +1,36 @@
 #pragma once
+#include"Vec2.h"
+
+
 class Player
 {
-};
+public:
+	Player();
+	~Player();
 
+	void Init();
+	void End();
+	void Update();
+	void Draw();
+
+	float GetPosX();
+
+	float GetPosY();
+
+	float GetRadius();
+
+	void SetHandle(int handle);
+
+	//アイテムに当たったらHP回復
+	void Regene() { m_pHp++; }
+private:
+	int m_handle;    //グラフィックのハンドル
+private:
+	int m_pHp;
+	//Playerのボム所持数
+	int m_bombNum;
+
+	Vec2 m_pos;
+
+	int m_radius;
+};
